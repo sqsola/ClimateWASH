0a14447c67072630221e6840c841091c976b11f9
sqsola <sqsola@gmail.com>
2023-11-23 21:48:54 -0500

    Added in Ethiopian Date conversion and celsius conversion. Corrected weather calculations for accumulations and instantaneous variables. Allowed for easier changing of data sets and locations for datasets (personal/HPC).

--- Weather_Extraction.R
+++ Weather_Extraction.R
@@ -1,33 +1,57 @@
+# Set the location
+#location <- "personal"
+location <- "HPC"
+
 # Load Packages
 library(plyr)
 library(tidyverse)
 library(lubridate)
 library(haven)
 library(magrittr)
-library(ethiopianDate, lib.loc = "/home/ssola1/rlibs/R")
 library(sf)
-library(lutz, lib.loc = "/home/ssola1/rlibs/R")
 library(ncdf4)
 library(shinythemes)
-library(kgc, lib.loc = "/home/ssola1/rlibs/R")
 
-# Year / Name of the Survey
+if (location == "personal") {
+library(ethiopianDate)
+library(lutz)
+library(kgc)
+  }
+ 
+if (location == "HPC") { 
+library(ethiopianDate, lib.loc = "/home/ssola1/rlibs/R")
+library(lutz, lib.loc = "/home/ssola1/rlibs/R")
+library(kgc, lib.loc = "/home/ssola1/rlibs/R")
+  }
+  
+# Specify the files to work on
 country <- "Ethiopia"
-name_year <- "ET_19"
-hh_filename <- "ETHR81FL"
-spatial_filename <- "ETGE81FL"
+name_year <- "ET_05"
 
 # Start system time
 Start <- Sys.time()
 
-# Set Working Directory
+if (location == "personal") {
+# Set Working Directory (Personal)
+setwd(paste0("C:/Users/Steven Sola/OneDrive - Johns Hopkins/Dissertation/Dissertation/Data/Aim 1/",
+             country,"/",name_year))
+  }
+
+if (location == "HPC") { 
+# Set Working Directory (HPC)
 setwd(paste0("~/data-mdavis65/steven_sola/",country,"/",name_year))
+  }
 
-# Read in survey data
-hh <- read_dta(paste0(name_year,"_hh/",hh_filename,".DTA"))
+#Read in survey data (Household member)
+files <- list.files(pattern=c(".DTA|\\.shp$"), recursive = TRUE)
 
-# Read in the coordinates for the data
-spatial <- st_read(paste0(name_year,"_gps/",spatial_filename,".shp"))
+# Filter the list for the Household level, save to environment
+file_hh <- str_subset(files, "HR")
+hh <- read_dta(file_hh)
+
+# Filter the list for the Spatial, save to environment
+file_spatial <- str_subset(files, "GE")
+spatial <- st_read(file_spatial)
 
 # Remove empty rows of HH dataset
 hh <- hh[,colSums(is.na(hh))<nrow(hh)]
@@ -41,32 +65,38 @@ full_survey$monthinterview <- full_survey$monthinterview %m+% months(full_survey
 # Add Date to the interview month
 full_survey$dateinterview <- full_survey$monthinterview + (full_survey$hv016-1)
 
+# This section will run if the country is Ethiopia
+# This will convert the dates from the Ethiopian calendar to Gregorian calendar
+if (country == "Ethiopia") {
+
 # Create new variables so that R doesn't get confused with lubridate functions.
 full_survey <- full_survey %>% 
-                    mutate(year_eth = hv007, month_eth = month(monthinterview), day_eth = day(dateinterview))
+  mutate(year_eth = hv007, month_eth = month(monthinterview), day_eth = day(dateinterview))
 
 # Create Gregorian date based off Ethiopian date
 full_survey <- full_survey %>% 
-                     mutate(dateinterview_greg = ethiopianToGregorian(year = as.numeric(.$year_eth),
-                                                                      month = as.numeric(.$month_eth),
-                                                                      date = as.numeric(.$day_eth)))
+  mutate(dateinterview_greg = ethiopianToGregorian(year = as.numeric(.$year_eth),
+                                                   month = as.numeric(.$month_eth),
+                                                   date = as.numeric(.$day_eth)))
 
-# Check created dates
-table(full_survey$dateinterview_greg)
-
-full_survey$dateinterview_greg <- as.Date(full_survey$dateinterview_greg)
+# Ensure the date is in Date format
+full_survey$dateinterview <- as.Date(full_survey$dateinterview_greg)
 
 # Push the new dates to the old date variables
-full_survey$year_eth <- year(full_survey$dateinterview_greg)
-full_survey$month_eth <- month(full_survey$dateinterview_greg)
-full_survey$day_eth <- day(full_survey$dateinterview_greg)
+full_survey$hv007 <- year(full_survey$dateinterview)
+full_survey$hv006 <- month(full_survey$dateinterview)
+full_survey$hv016 <- day(full_survey$dateinterview)
+
+print("Dates were converted from the Ethiopian to the Gregorian Calendar")
+
+  }
 
 # 60 Days before date of the interview
-full_survey$dateinterview_minus60 <- full_survey$dateinterview_greg - 60
+full_survey$dateinterview_minus60 <- full_survey$dateinterview - 60
 full_survey$dateinterview_minus60 <- as.Date(full_survey$dateinterview_minus60, format = "%Y-%m-%d")
 
 # 1 day after the date of the interview
-full_survey$dateinterview_plus1 <- full_survey$dateinterview_greg + 1
+full_survey$dateinterview_plus1 <- full_survey$dateinterview + 1
 full_survey$dateinterview_plus1 <- as.Date(full_survey$dateinterview_plus1, format = "%Y-%m-%d")
 
 # Remove duplicates
@@ -80,14 +110,14 @@ spatial <- spatial %>% filter(!between(LATNUM, -0.0001, 0.0001))
 
 # Ensure that the Spatial dataset year is only two characters (last two)
 spatial$DHSYEAR <- sprintf('%02d', spatial$DHSYEAR %% 100) %>% as.numeric
-full_survey$year_eth <- sprintf('%02d', full_survey$year_eth %% 100) %>% as.numeric
+full_survey$hv007 <- sprintf('%02d', full_survey$hv007 %% 100) %>% as.numeric
 
 # join spatial to data
-full_survey_spatial <- left_join(full_survey, spatial, by = c("hv001" = "DHSCLUST", "year_eth" = "DHSYEAR"))
+full_survey_spatial <- left_join(full_survey, spatial, by = c("hv001" = "DHSCLUST", "hv007" = "DHSYEAR"))
 
 # Get unique combos of lat, long, and dates
 unique_obs <- unique(full_survey_spatial[c("LATNUM", "LONGNUM", "hv001", 
-                                         "dateinterview_greg", "dateinterview_minus60",
+                                         "dateinterview", "dateinterview_minus60",
                                          "dateinterview_plus1")])
 
 # Remove rows with 0 lat and long
@@ -96,10 +126,9 @@ unique_obs <- unique_obs %>% filter(LATNUM != 0 | LONGNUM != 0)
 # Create list for the For Loop
 tz_list <-  list()
 
-# Get the time zones
 for (time in 1:nrow(unique_obs)) {
-  tz_list[[time]] <- tz_offset(unique_obs$dateinterview_greg[[time]],
-                               tz = tz_lookup_coords(unique_obs$LATNUM[[time]], unique_obs$LONGNUM[[time]],
+  tz_list[[time]] <- tz_offset(unique_obs$dateinterview[[time]], 
+                               tz = tz_lookup_coords(unique_obs$LATNUM[[time]], unique_obs$LONGNUM[[time]], 
                                                      method = "fast", warn = FALSE))
 }
 
@@ -115,13 +144,12 @@ files <- Sys.glob(file.path("~/data-mdavis65/steven_sola/1_Weather", "*.nc"))
 # Empty frame to bind data
 weather_survey <- list()
 
-# Start the massive for loop
 for (obs in 1:nrow(unique_obs)) {
   
   # Change the format to month, date, year
   # MAY NOT NEED TO CHANGE THE FORMATTING WITH LUBRIDATE
   date_start <- unique_obs$dateinterview_minus60[[obs]]
-  date_end <- unique_obs$dateinterview_greg[[obs]]
+  date_end <- unique_obs$dateinterview[[obs]]
   
   # Set the lat/long
   lat <- unique_obs$LATNUM[[obs]]
@@ -135,8 +163,9 @@ for (obs in 1:nrow(unique_obs)) {
   files_subset <- str_subset(files, paste0(match_dates, collapse = '|')) %>% rev()
   
   # Variables to extract from data
-  variables <- c("t2m", "skt", "e", "sro", "ssro", 
-                 "tp", "lai_lv", "lai_hv", "d2m")
+  accum_var <- c("e", "sro", "ssro", "tp")
+  instant_var <- c("t2m", "skt", "lai_lv", "lai_hv", "d2m")
+  variables <- c(accum_var, instant_var)
   
   # UTC Offset Number
   utc_offset <- unique_obs$utc_offset_h[[obs]]
@@ -208,41 +237,76 @@ for (obs in 1:nrow(unique_obs)) {
   }
   
   # Bind all the variables from all the files together, transpose, save as df
-  weather_60day_0 <- do.call(rbind, weather_60day_0) %>% t() %>% as.data.frame()
-  weather_60day_1 <- do.call(rbind, weather_60day_1) %>% t() %>% as.data.frame()
+  weather_60day_0 <-  do.call(rbind, weather_60day_0) %>% t() %>% as.data.frame()
+  weather_60day_1 <-  do.call(rbind, weather_60day_1) %>% t() %>% as.data.frame()
   weather_60day_48hr <- rbind(weather_60day_0, weather_60day_1)
   
   # Rename the column names
+  colnames(weather_60day_0) <- make.unique(names(weather_60day_0), sep = "_")
+  colnames(weather_60day_1) <- make.unique(names(weather_60day_1), sep = "_")
   colnames(weather_60day_48hr) <- make.unique(names(weather_60day_48hr), sep = "_")
   
   # add UTC offset
   weather_60day_48hr <- cbind(utc_offset, weather_60day_48hr)
-
-  # Take the UTC number, and extract the 24 hours following that number
-  weather_60day_tzcor <- weather_60day_48hr[(weather_60day_48hr$utc_offset[1]):(weather_60day_48hr$utc_offset[1]+24),]
   
-  # Create a new row with the column means
-  weather_60day_tzcor <- weather_60day_tzcor %>% dplyr::summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)))
+  # Separate out the accumulations and the instantaneous variables
+  accum <- weather_60day_48hr %>% select(starts_with(accum_var))
+  instant <- weather_60day_48hr %>% select("utc_offset", starts_with(instant_var))
+  
+  # ACCUMULATION VARIABLES #
+    # If the Offset is 0, just take the Day 1 cumulative
+    # If the Offset is 1 or more:
+    #  - Take the Day 1 cumulative
+    #  - Take the number of offset hours to decide number of rows for 2nd day
+    #  - From the second day, take the last row.
+    #  - per ECMWFR, accumulations are from beginning to end of step
+    #  - Add the totals from Day 1 and Day 2
+    
+    if (utc_offset == 0) {
+          accum <- accum %>% slice(25)
+    } else if (utc_offset >= 1) {
+          accum_day1 <- accum %>% slice(25)
+          
+          accum_day2 <- accum[25:(utc_offset+25),] %>% 
+          slice_tail(n = 1) 
+          
+          accum <- rbind(accum_day1, accum_day2) %>% 
+                            summarise(across(everything(), ~ last(.x) + first(.x)))
+    }
+
+  # INSTANTANEOUS VARIABLES #
+  # Take the UTC number, and extract the 24 hours following that number
+  instant <- instant[(instant$utc_offset[1]):(instant$utc_offset[1]+24),] %>% 
+                            summarise(across(everything(), ~ mean(.x, na.rm = TRUE)))
+  
+  # Combine the accumulation and instantaneous variables
+  weather_60day_tzcorrect <- cbind(instant, accum)
+  
+  # Check whether lengths are equal
+  stopifnot(length(weather_60day_tzcorrect) == length(weather_60day_48hr))
   
   # Add in previous variables from survey data
-  weather_survey <- rbind(weather_survey, weather_60day_tzcor)
+  weather_survey <- rbind(weather_survey, weather_60day_tzcorrect)
 
   # Progress Report
   cat(paste0(round(obs / nrow(unique_obs) * 100), "% completed", ", Elapsed Time: ", 
              round(as.difftime(Sys.time()-Start, format = "%T"), digits = 2), "\n"))
-  
   }
 
 # Bind processed data back to original df
 weather_survey_coords <- cbind(unique_obs, weather_survey)
 
-# Assign Koppen-Geiger Zone
-weather_survey_coords <- weather_survey_coords %>% 
-  mutate(rndCoord.lon = RoundCoordinates(.$LONGNUM, res = "course", latlong = "lon"),
-         rndCoord.lat = RoundCoordinates(.$LATNUM, res = "course", latlong = "lat"))
+# Convert Kelvin to Celsius for skin temp, 2m temp, 2m dewpoint
+weather_survey_coords_cel <- weather_survey_coords %>% 
+                                mutate_at(vars(starts_with(c("skt","t2m","d2m"))),  ~.x - 273.15)
 
-weather_final <- weather_survey_coords %>% 
-  mutate(kgc = LookupCZ(., res = "course", rc = FALSE))
+# Assign Köppen-Geiger Zone
+weather_survey_coords_cel <- weather_survey_coords_cel %>% 
+                                  mutate(rndCoord.lon = RoundCoordinates(.$LONGNUM, res = "course", latlong = "lon"),
+                                  rndCoord.lat = RoundCoordinates(.$LATNUM, res = "course", latlong = "lat"))
+
+weather_final <- weather_survey_coords_cel %>% 
+                      mutate(kgc = LookupCZ(., res = "course", rc = FALSE))
 
 # Save the outputs
 write.csv(weather_final, paste0("~/data-mdavis65/steven_sola/2_Weather_Processed/CSV/", name_year,"_weatherfinal.csv"))
