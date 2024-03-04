c344d02bd00d82089369574dc46150db5141d51b
sqsola <sqsola@gmail.com>
2024-03-03 21:37:13 -0500

    Sent more old code to the graveyard. good riddance.

--- CodeGraveyard.R
+++ CodeGraveyard.R
@@ -60,3 +60,94 @@ data <- data %>% mutate(hv201_class =
                             dhs_phase >= 2 & dhs_phase <= 8 & hv201 == 99 ~ NA,
                             .default = "CHECK THIS OUT"
                           ))  
+
+
+
+
+
+
+
+
+
+
+
+plot_rural <- ggplot(rural, aes(x=hv204)) + 
+  geom_line(aes(y = tp_totalminus30), color = "steelblue") + 
+  geom_line(aes(y = e_totalminus30), color="darkred") +
+  xlab("Time to Collect Water") + ylab("Inches in the Previous Month") +
+  scale_x_continuous(expand = c(0,0), limits = c(0,300)) +
+  scale_y_continuous(expand = c(0,0), limits = c(-0.3,3)) +
+  theme(axis.text = element_text (size = 14, face = "bold")) +
+  theme(axis.title = element_text(size = 14, face = "bold")) 
+
+plot_rural
+
+
+rural$e_avgminus30
+
+
+
+
+plot_dry <- ggplot(dry, aes(x=hv204)) + 
+  geom_line(aes(y = precip_minus1mototal_inchday), color = "steelblue") + 
+  geom_line(aes(y = evap_minus1mototal_inchday), color="darkred") +
+  xlab("Time to Collect Water") + ylab("Inches in the Previous Month") +
+  scale_x_continuous(expand = c(0,0), limits = c(0,300)) +
+  scale_y_continuous(expand = c(0,0), limits = c(-0.1,18)) +
+  theme(axis.text = element_text (size = 14, face = "bold")) +
+  theme(axis.title = element_text(size = 14, face = "bold")) 
+
+plot_dry
+
+
+
+
+
+
+
+# See if the amount of piped water changes by the year of the survey
+test <- rural_pipe_nona %>% 
+  group_by(hv007) %>%
+  dplyr::summarise(
+    "num_households" = n(),
+    across(piped, list(Yes = ~ sum(. == "Piped"),
+                       No  = ~ sum(. == "Not Piped"))))
+test$Percent_Piped <- (test$piped_Yes / test$num_households) * 100
+test
+
+
+# Count the number of PSUs
+rural_final %>% group_by(hv001) %>% dplyr::summarise(num_psu = n()) %>% dplyr::summarise(sum_psu = max(hv001))
+
+
+
+
+
+# Person fetching water
+table(rural_final$hv236)
+
+rural_final %>% group_by(hv236) %>% 
+  dplyr::summarise(mean_evap_total = mean(evap_overalltotal_inchday),
+                   mean_precip_total = mean(precip_overalltotal_inchday),
+                   mean_precip_minus1 = mean(precip_minus1mototal_inchday),
+                   mean_walk = mean(hv204),
+                   num_households = n())
+
+rural_final %>% filter(hv236 > 4 | is.na(hv236)) %>% 
+  dplyr::summarise(mean_walk = mean(hv204),
+                   num_households = n())
+
+
+
+
+## FOR MAPPING
+
+mapping <- rural_final %>% select(ID, LONGNUM, LATNUM)
+
+mapping <- unique(mapping[c("LATNUM", "LONGNUM")])
+
+mapping$ID <- NA
+
+mapping$ID <- seq(1:nrow(mapping))
+
+write.csv(mapping, "mappingforposter.csv")
